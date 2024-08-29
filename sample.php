b9ec1293713826440f242b40aa276806e21d6122
ShotaArima <g2172010@tcu.ac.jp>
2024-08-29 21:31:43 +0900

    failure not write log file and not work cron

---
+++ sample.php
@@ -0,0 +1,23 @@
+<?php
+
+//// 引数を受け取る
+//$current_time = null; // 初期化
+//if (isset($argv[1])) {
+//    try {
+//        $current_time = (new \Datetime($argv[1]))->format('Y-m-d H:i:s');
+//    } catch (Exception $e) {
+//        // 日付のフォーマットが無効な場合のエラーハンドリング
+//        $current_time = 'Invalid date format: ' . $argv[1];
+//    }
+//} else {
+//    $current_time = 'No argument provided.';
+//}
+//
+//// 出力ファイルのパスを設定
+//$output_file_path = '/Users/shota/PhpstormProjects/demo-cron/file/cron.log';
+//
+//// ログの出力をする
+//error_log('Current time: ' . $current_time . PHP_EOL, 3, $output_file_path);
+
++
+print("Hello World!\n");
